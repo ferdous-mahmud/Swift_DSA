d0d89c5fa269c41b300225da0c09590f7ed6cfdc
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-06-21 21:16:29 +0600

    CycleRotation edge case added

--- practice/2-Arrays/Q1-CyclicRotation-Question.playground/Contents.swift
+++ practice/2-Arrays/Q1-CyclicRotation-Question.playground/Contents.swift
@@ -15,10 +15,16 @@ import UIKit
      [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
  
  Time Complexity: O(N^K)
+ Space Complexity: O(N)
 
  */
 
 func solution(A: [Int], K: Int) -> [Int] {
+    
+    // Negative and 0 rotation OR empty array
+    if 0 >= K || A.isEmpty {
+        return A
+    }
     
     var result = [Int]()
     var temp = A
@@ -38,9 +44,10 @@ func solution(A: [Int], K: Int) -> [Int] {
     return result
 }
 
-solution(A: [1, 2, 3, 4, 5], K: 1) // 5 1 2 3 4
+solution(A: [1, 2, 3, 4, 5], K: 0) // 1 2 3 4 5
 solution(A: [1, 2, 3, 4, 5], K: 2) // 4 5 1 2 3
 solution(A: [1, 2, 3, 4, 5], K: 3) // 3 4 5 1 2
 
 solution(A: [3, 8, 9, 7, 6], K: 3) // [9, 7, 6, 3, 8]
+solution(A: [], K: -3) // []
 
