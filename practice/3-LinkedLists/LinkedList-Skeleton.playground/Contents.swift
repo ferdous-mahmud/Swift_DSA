9946ba69b02eaf8a7e87c800220c8224cdd6bafa
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-06-23 21:56:18 +0600

    linkedlist functions added

--- practice/3-LinkedLists/LinkedList-Skeleton.playground/Contents.swift
+++ practice/3-LinkedLists/LinkedList-Skeleton.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Darwin
 
 class Node {
     var data: Int
@@ -10,77 +11,168 @@ class Node {
     }
 }
 
-let node3 = Node(30)
-let node2 = Node(20, node3)
-let node1 = Node(10, node2) // Head
+//let node3 = Node(30)
+//let node2 = Node(20, node3)
+//let node1 = Node(10, node2) // Head
 
-func printLinkedList (_ head: Node?){
-    if head == nil {
-        return
+
+class LinkList {
+    private var head: Node?
+
+    // O(1)
+    func addFront(_ data: Int) {
+        let newNode = Node(data, head)
+        head = newNode
     }
-    
-    var node = head
-    print(node!.data)
-    
-    while node!.next != nil {
-        print(node!.next!.data)
-        node = node?.next
+
+    // O(1)
+    func getFirst() -> Int? {
+        return head?.data
+    }
+
+    // O(n)
+    func addBack(_ data: Int) {
+        let newNode = Node(data)
+        
+        if head == nil {
+            head = newNode
+            return
+        }
+        
+        var node = head!
+        while node.next != nil {
+            node = node.next!
+        }
+        
+        node.next = newNode
+    }
+
+    func getLast() -> Int? {
+        var node = head!
+        
+        while node.next != nil {
+            node = node.next!
+        }
+        return node.data
+    }
+
+    // O(n)
+    func insert(position: Int, data: Int) {
+        
+        if position == 0 {
+            addFront(data)
+            return
+        }
+        
+        
+        let newNode = Node(data)
+        var currentPosition = 0
+        var node = head!
+        
+        while currentPosition != position - 1 {
+            node = node.next!
+            currentPosition += 1
+        }
+        
+        let temp = node.next
+        node.next = newNode
+        node = node.next!
+        node.next = temp
+    }
+
+    // O(1)
+    func deleteFirst() {
+        head = head?.next
+    }
+
+    // O(n)
+    func deleteLast() {
+        var node = head!
+        var previousNode: Node?
+        
+        while node.next != nil {
+            previousNode = node
+            node = node.next!
+        }
+        previousNode?.next = nil
+    }
+
+    // O(n)
+    func delete(at position: Int) {
+        var n = position
+        var node = head
+        var previousNode: Node?
+        
+        while n != 0 {
+            previousNode = node
+            node = node?.next
+            n -= 1
+        }
+        previousNode?.next = node?.next
+    }
+
+    // O(1)
+    var isEmpty: Bool {
+        if head != nil {
+            return false
+        }
+        return true
+    }
+
+    // O(1)
+    func clear() {
+        head = nil
+    }
+
+    // O(n)
+    func printLinkedList() {
+        if head == nil { return }
+
+        var result = [Int]()
+        var node = head
+        result.append(node!.data)
+
+        while node?.next != nil {
+            result.append(node!.next!.data)
+            node = node?.next
+        }
+
+        print(result)
     }
 }
 
-printLinkedList(node1)
+let linkedList = LinkList()
 
-//class LinkList {
-//    private var head: Node?
-//
-//    func addFront(_ data: Int) {
-//    }
-//
-//    func getFirst() -> Int? {
-//        return 0
-//    }
-//
-//    func addBack(_ data: Int) {
-//    }
-//
-//    func getLast() -> Int? {
-//        return nil
-//    }
-//
-//    func insert(position: Int, data: Int) {
-//    }
-//
-//    func deleteFirst() {
-//    }
-//
-//    func deleteLast() {
-//    }
-//
-//    func delete(at position: Int) {
-//    }
-//
-//    var isEmpty: Bool {
-//        return false
-//    }
-//
-//    func clear() {
-//    }
-//
-//    func printLinkedList() {
-//        if head == nil { return }
-//
-//        var result = [Int]()
-//        var node = head
-//        result.append(node!.data)
-//
-//        while node?.next != nil {
-//            result.append(node!.next!.data)
-//            node = node?.next
-//        }
-//
-//        print(result)
-//    }
-//}
-//
-//let linkedList = LinkList()
+linkedList.addFront(30)
+linkedList.addFront(20)
+linkedList.addFront(10)
+
+
+linkedList.addBack(40)
+linkedList.addBack(50)
+linkedList.addBack(60)
+
+linkedList.insert(position: 3, data: 100)
+linkedList.insert(position: 4, data: 90)
+
+linkedList.deleteFirst()
+linkedList.deleteLast()
+
+print(linkedList.isEmpty)
+
+print("First : \(linkedList.getFirst()!)")
+print("Last: \(linkedList.getLast()!)")
+
+
+linkedList.printLinkedList()
+
+linkedList.clear()
+linkedList.insert(position: 0, data: 10)
+linkedList.insert(position: 1, data: 20)
+linkedList.insert(position: 2, data: 30)
+
+linkedList.delete(at: 1)
+linkedList.delete(at: 1)
+
+linkedList.printLinkedList()
 
